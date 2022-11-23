1f52e3f665ebb7cb2b0d0767d3d2ba4c684f5dff
Sid Hathi <sidhathi20@gmail.com>
2022-11-23 13:37:02 -0800

    Added utility functions to search domcontent for lease term lists and keywords

--- ScrapingUtils.py
+++ ScrapingUtils.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from bs4 import BeautifulSoup
 from ParsingModel import TagModel
+import re
+from QueryParams import LeaseTerm
 
 async def htmlPull(url: str, timeout: int) -> str:
     opts = ChromeOptions()
@@ -22,3 +24,50 @@ def followTagMap(tagMap: list[TagModel], dom: BeautifulSoup) -> list[BeautifulSo
             nextTagList.extend(tag.find_all(step.tagType, step.identifiers))
         currentTagList = nextTagList
     return currentTagList
+
+def findIntegerMonths(domContent: str) -> list[int] | None:
+    regex = re.compile(r'\d+[\s, \A\Z](month|months)')
+    matchingSubstrings: list[str] = regex.findall(domContent)
+    monthVals: list[int] = []
+    added: set = set()
+    if len(matchingSubstrings) < 1:
+        return None
+    for match in matchingSubstrings:
+        split = match.split(' ')
+        assert(len(split) > 0)
+        val = int(split[0])
+        if val not in added:
+            monthVals.append(val)
+            added.add(val)
+    return monthVals
+
+def findIntegerListMonths(domContent: str) -> list[int] | None:
+    regex = re.compile(r'[\s\d]+(,[\s\d]+)*\s*(or|and)*\s*\d*\s(month|months)')
+    matchingSubstrings: list[str] = regex.findall(domContent)
+    monthVals: list[int] = []
+    added: set = set()
+    if len(matchingSubstrings) < 1:
+        return None
+    for match in matchingSubstrings:
+        split = match.split(',')
+        assert(len(split) > 0)
+        for splitStr in split:
+            valStr = re.sub(r'[^0-9]', '', splitStr)
+            if len(valStr) > 1:
+                val = int(valStr)
+                if val not in added:
+                    monthVals.append(val)
+                    added.add(val)
+    return monthVals
+
+def matchKeyword(domContent: str, keyword: str) -> bool:
+    if keyword in domContent:
+        return True
+    return False
+
+def matchLeaseTermByKeyword(domContent: str, keyDict: dict[str, LeaseTerm]) -> list[LeaseTerm]:
+    matches: list[LeaseTerm] = []
+    for keyword in keyDict:
+        if matchKeyword(domContent, keyword):
+            matches.append(keyDict[keyword])
+    return matches    
