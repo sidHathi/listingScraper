8025276bcf14dda6b493e30096b0475e68af9774
Sid Hathi <sidhathi20@gmail.com>
2022-12-14 23:54:43 -0500

    added cronjob files

--- app/Scrape/App.py
+++ app/Scrape/App.py
@@ -50,7 +50,7 @@ class App:
             requestHub=self.requestHub
         )
 
-        return [fbmScraper]
+        return [fbmScraper, rentScraper]
 
     async def run(self):
         queries: list[Query] = self.getQueries()
