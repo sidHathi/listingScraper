f60505b080f595fb1047bbe328b0ceb36402fc5a
Sid Hathi <sidhathi20@gmail.com>
2023-01-18 16:04:34 -0500

    added blacklist usage to main scraper app

--- app/Scrape/__main__.py
+++ app/Scrape/__main__.py
@@ -2,6 +2,7 @@ import sys
 import asyncio
 from typing import TextIO
 
+from ..utils.requestUtils import buildBlacklist
 from ..loggers.RequestLogger import RequestLogger
 from ..DBInterface import DBInterface
 from ..RequestHub import RequestHub
@@ -9,12 +10,17 @@ from .App import App
 
 
 async def main() -> None:
+    blacklist: dict[str, set[str]] | None = buildBlacklist()
     scrapeLogFile: TextIO = open('scrapeLog.log', 'w+')
     requestLogFile: TextIO = open('requestLog.log', 'w+')
+    blacklistLogFile: TextIO = open('blacklist.log', 'w+')
 
     requestLogger: RequestLogger = RequestLogger(requestLogFile)
     dbInterface: DBInterface = DBInterface()
     requestHub: RequestHub = RequestHub(requestLogger)
+    if blacklist is not None:
+        requestLogger.addBlacklist(blacklist, blacklistLogFile)
+        requestHub.addBlacklist(blacklist)
     app: App = App(dbInterface, requestHub, scrapeLogFile)
 
     await app.run()
